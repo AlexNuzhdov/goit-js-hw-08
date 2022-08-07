9e85e65d611f8886b8f8d54b1f1edd5828a3c437
AlexNuzhdov <100243457+AlexNuzhdov@users.noreply.github.com>
2022-08-07 18:01:58 +0000

    Deploying to gh-pages from @ AlexNuzhdov/goit-js-hw-08@77a34bfc98d24d6f829775e3b712f096001e97f3 🚀

--- 03-feedback.c57ec3fb.js
+++
@@ -1,2 +0,0 @@
-!function(){var e={form:document.queryselector(".feedback-form"),textarea:document.querySelector(".feedback-form  textarea")};e.form.addEventListener("submit",(function(e){})),e.textarea.addEventListener("input",(function(e){var t=e.currentTarget.Value;сonsole.log(t)}))}();
-//# sourceMappingURL=03-feedback.c57ec3fb.js.map
--- 03-feedback.cd9bb16a.js
+++
@@ -1,2 +0,0 @@
-const e={form:document.queryselector(".feedback-form"),textarea:document.querySelector(".feedback-form  textarea")};e.form.addEventListener("submit",(function(e){})),e.textarea.addEventListener("input",(function(e){const t=e.currentTarget.Value;сonsole.log(t)}));
-//# sourceMappingURL=03-feedback.cd9bb16a.js.map
