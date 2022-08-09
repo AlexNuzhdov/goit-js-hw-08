c9fb6e7a22797ca946759521150ee6dcd6013435
AlexNuzhdov <alexnuzhdov90@gmail.com>
2022-08-09 17:56:29 +0300

    fixed feedback2

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -12,29 +12,29 @@ const refs = {
      const STORAGE_KEY = "feedback-form-state";                           
      const formData = {};
 
-    function onTextareaInput(event) {
+     function onTextareaInput(event) {
        formData[event.target.name] = event.target.value;
        localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
    };
                                                                      
-    function onFormSubmit(event) {
-        event.preventDefault();                                             
+     function onFormSubmit(event) {
+        event.preventDefault(); 
+        refs.email = event.currentTarget.email.value;
+        refs.message = event.currentTarget.message.value;
+
        if (refs.email === '' || refs.message === '') {
           return alert('All fields must be filled!');
         }
-          refs.email = event.currentTarget.email.value;
-          refs.message = event.currentTarget.message.value;
           console.log(JSON.parse(localStorage.getItem(STORAGE_KEY)));
           event.currentTarget.reset();                                      
           localStorage.removeItem(STORAGE_KEY);      
         
    };
-                                                      
-   function populateTextarea() {
+      function populateTextarea() {
        const savedMessage = JSON.parse(localStorage.getItem(STORAGE_KEY)); 
        if (savedMessage) {
          refs.email.value = savedMessage.email;
          refs.message.value = savedMessage.message;
     }
    };
-        populateTextarea();
+       populateTextarea();
